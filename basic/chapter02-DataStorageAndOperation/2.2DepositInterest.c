60fbc7db79765e60fbc67753300841659e2873bc
HaiTao-Lian <1654415895@qq.com>
2023-12-01 21:36:00 +0800

    add practices of chapter02

---
+++ basic/chapter02-DataStorageAndOperation/2.2DepositInterest.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+int main() {
+  double r0, r1, r2, r3, r5;
+  r0 = 0.0072; // season
+  r1 = 0.0414; // one year
+  r2 = 0.0468; // two years
+  r3 = 0.054;  // three years
+  r5 = 0.0585; // five years
+
+  double p1 = 1000 * (1 + 5 * r5);
+  double p2 = 1000 * (1 + 2 * r2) * (1 + 3 * r3);
+  double p3 = 1000 * (1 + 3 * r3) * (1 + 2 * r2);
+  double p4 = pow((1 + r1), 5) * 1000;
+  double p5 = pow((1 + r0 / 4), 4 * 5) * 1000;
+
+  printf("The plan A: %10.2f\n", p1);
+  printf("The plan B: %10.2f\n", p2);
+  printf("The plan C: %10.2f\n", p3);
+  printf("The plan D: %10.2f\n", p4);
+  printf("The plan E: %10.2f\n", p5);
+
+  return 0;
+}
---
+++ basic/chapter02-DataStorageAndOperation/2.3TransToPassword.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+  char c1 = 'C';
+  char c2 = 'h';
+  char c3 = 'i';
+  char c4 = 'n';
+  char c5 = 'a';
+
+  printf("%c = %c\n", c1, c1 + 4);
+  printf("%c = %c\n", c2, c2 + 4);
+  printf("%c = %c\n", c3, c3 + 4);
+  printf("%c = %c\n", c4, c4 + 4);
+  printf("%c = %c\n", c5, c5 + 4);
+  return 0;
+}
