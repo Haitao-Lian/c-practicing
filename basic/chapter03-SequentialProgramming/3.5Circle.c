2b3e38926c8d9af2cb3b25f7cf875f4af3f1a633
HaiTao-Lian <1654415895@qq.com>
2023-12-03 21:05:05 +0800

    add practices of chapter03

---
+++ basic/chapter03-SequentialProgramming/3.5Circle.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define PI 3.14
+#include <math.h>
+#include <stdio.h>
+int main() {
+  float r, height, circumference, circelArea, sphereSArea, sphereV, cylinderV;
+  scanf("r=%f, h=%f", &r, &height);
+
+  circumference = 2 * PI * r;
+  circelArea = PI * r * r;
+  sphereSArea = 4 * PI * r * r;
+  sphereV = 3.0 / 4.0 * PI * pow(r, 3);
+  cylinderV = PI * r * r * height;
+
+  printf("circumference = %5.2f\n", circumference);
+  printf("circelArea = %5.2f\n", circelArea);
+  printf("sphereSArea = %5.2f\n", sphereSArea);
+  printf("sphereV = %5.2f\n", sphereV);
+  printf("cylinderV = %5.2f\n", cylinderV);
+
+  return 0;
+}
---
+++ basic/chapter03-SequentialProgramming/3.6Temperature.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main(){
+    float c, F;
+    printf("Enter a Fahrenheit temperature: ");
+    scanf("%f", &F);
+
+    c = 5.0/9.0 * (F - 32);
+    printf("After converting to degrees Celsius: %5.2f\n", c);
+    return 0;
+}
