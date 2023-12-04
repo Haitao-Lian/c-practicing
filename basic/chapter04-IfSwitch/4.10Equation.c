c8d3222b202468eb39b24feb80229ef6bb7c0090
HaiTao-Lian <1654415895@qq.com>
2023-12-04 18:34:18 +0800

    add practices of chapter04

---
+++ basic/chapter04-IfSwitch/4.10Equation.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <math.h>
+#include <stdio.h>
+int main() {
+  float a, b, c, x1, x2, real, image;
+  printf("Enter a, b, c: ");
+  scanf("%f,%f,%f", &a, &b, &c);
+
+  float t = b * b - 4 * a * c;
+
+  if (fabs(a) <= 1e-6) {
+    printf("This is nor a quadratic\n");
+  } else {
+    if (fabs(t) <= 1e-6) {
+      x1 = ((-b) + sqrt(t)) / 2 * a;
+      x2 = ((-b) - sqrt(-1 * t)) / 2 * a;
+      printf("x1 = %f, x2 = %f\n", x1, x2);
+    } else {
+      real = -b / (2 * a);
+      image = sqrt(-t) / (2 * a);
+      printf("x1 = %8.4f + %8.4fi\n", real, image);
+      printf("x2 = %8.4f - %8.4fi\n", real, image);
+    }
+  }
+  return 0;
+}
---
+++ basic/chapter04-IfSwitch/4.9RoundTower.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float x, y;
+    int l;
+
+    printf("Enter the coordinates of the building: ");
+    scanf("(%f,%f)", &x, &y);
+    if (x >= 0) {
+        if (y >=0 ) {
+            l = pow((x - 2), 2) + pow((y - 2), 2);
+        }else {
+            l = pow((x - 2), 2) + pow((y + 2), 2);
+        }
+    }else {
+        if (y >= 0) {
+            l = pow((x + 2), 2) + pow((y - 2), 2);
+        }else {
+            l = pow((x + 2), 2) + pow((y + 2), 2);
+        }
+    }
+
+    if (l > 1 ) {
+        printf("height of building: 0m\n");
+    }else {
+        printf("height of building: 10m\n");
+    }
+    return 0;
+}
