353744f7b4bf73780d258f1a7296ad837e946dd4
HaiTao-Lian <1654415895@qq.com>
2023-12-06 20:45:30 +0800

    add practices of chapter05(uncompleted)

---
+++ basic/chapter05-ForWhile/5.2CountChar.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main() {
+  int enCount = 0, spaceCount = 0, numCount = 0, otherCount = 0;
+  char c;
+
+  printf("Enter a line of chars: ");
+  for (c = getchar(); c != '\n';) {
+    if (c ) {
+    
+    }
+  }
+  return 0;
+}
