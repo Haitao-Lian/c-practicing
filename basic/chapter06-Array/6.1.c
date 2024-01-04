7602f506aa6c71ce55abb3b348ab55128cd9c775
HaiTao-Lian <1654415895@qq.com>
2024-01-04 12:24:56 +0800

    add new chapter of C Primer Plus

---
+++ basic/chapter06-Array/6.1.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main() {
+    double scores[10], sum = 0;
+    printf("Enter ten scores(use space to split): ");
+    for (int i = 0; i < 10; i++) {
+        scanf("%lf", &scores[i]);
+    }
+    for (int i = 0; i < 10; i++) {
+        printf("The score of %d is %.2lf\n", i + 1, scores[i]);
+        sum += scores[i];
+    }
+
+    printf("summary = %.2lf \naver = %.4lf \n", sum, sum / 10.0);
+    return 0;
+}
