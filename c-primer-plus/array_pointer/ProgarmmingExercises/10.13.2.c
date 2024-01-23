630228fb73016421b531c355dc11be232e1d0d8b
HaiTao-Lian <1654415895@qq.com>
2024-01-23 21:55:33 +0800

    add exercises

---
+++ c-primer-plus/array_pointer/ProgarmmingExercises/10.13.2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void copy_arr(double *tarr, double *sarr, int len);
+void copy_ptr(double *tarr, double *sarr, int len);
+void copy_ptrs(double *tarr, double *sarr, double *end);
+void pri_arr(double *arr, int len);
+
+int main() {
+    double source[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    double target1[5];
+    double target2[5];
+    double target3[5];
+
+    copy_arr(target1, source, 5);
+    copy_ptr(target2, source, 5);
+    copy_ptrs(target3, source, source + 5);
+
+    pri_arr(target1, 5);
+    pri_arr(target2, 5);
+    pri_arr(target3, 5);
+
+    return 0;
+}
+
+void copy_arr(double *tarr, double *sarr, int len) {
+    for (int i = 0; i < len; i++)
+        tarr[i] = sarr[i];
+}
+
+void copy_ptr(double *tarr, double *sarr, int len) {
+    for (int i = 0; i < len; i++) {
+        *(tarr++) = *(sarr++);
+        // *tarr = *sarr;
+        // tarr++;
+        // sarr++;
+    }
+}
+
+void copy_ptrs(double *tarr, double *sarr, double *end) {
+    while (sarr <= end) {
+        *(tarr++) = *(sarr++);
+        // *tarr = *sarr;
+        // tarr++;
+        // sarr++;
+    }
+}
+
+void pri_arr(double *arr, int len) {
+    printf("Traverse arrays:\n");
+    for (int i = 0; i < len; i++)
+        printf("%5.2f\n", arr[i]);
+}
