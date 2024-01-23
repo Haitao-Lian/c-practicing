575d33a318f78569f87a8501de086bac8d63190e
HaiTao-Lian <1654415895@qq.com>
2024-01-23 21:27:57 +0800

    add exercises of pointer and array.

---
+++ c-primer-plus/array_pointer/testConstPtr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // const int a = 4;
+    // int *pa = &a;
+    // int *pa1 = pa;
+    // printf("*pa = %d\n", *pa);
+
+    // const int arr1[] = {1,2,3};
+    // int arr2[]={2,3};
+    // const int *pi = arr2;
+    // printf("*pi = %d\n", *pi);
+    // pi = arr1;
+    // printf("*pi = %d\n", *pi);
+    // // *pi = 2;
+    // printf("*pi = %d\n", *pi);
+    return 0;
+}
