e778e96ad76f4a271e65e929c6a2825524421198
HaiTao-Lian <1654415895@qq.com>
2024-01-29 17:30:56 +0800

    add examples

---
+++ c-primer-plus/string/fgests1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define STLEN 14
+
+int main()
+{
+    char words[STLEN];
+
+    puts("Enter a string, please:\n");
+
+    return 0;
+}
--- c-primer-plus/string/strptr.c
+++ c-primer-plus/string/strptr.c
@@ -5,5 +5,10 @@ int main()
     // printf("%p %p %c %c", "we", "we" + 1, *"happy", *("happy" + 1));
     printf("%s, %p, %c\n", "We", "are", *"space farers");
 
+    char str[10];
+    printf("Ente a string:\n");
+    // gets(str); // unsafe!
+    puts(str);
+
     return 0;
 }
