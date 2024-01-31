f6e7eda7bdb0af286c2b1939771801c4fec82c3b
HaiTao-Lian <1654415895@qq.com>
2024-01-31 21:24:00 +0800

    add examples

--- c-primer-plus/string/fgests1.c
+++ c-primer-plus/string/fgests1.c
@@ -5,7 +5,20 @@ int main()
 {
     char words[STLEN];
 
-    puts("Enter a string, please:\n");
+    puts("Enter a string, please:"); // 自动打印回车
+    fgets(words, STLEN, stdin); // 读入一个回车，并停止读入。
+
+    printf("Your string twice (puts(), then fputs()):\n");
+    puts(words); // 会补上一个回车
+    fputs(words, stdout); // 不会不上回车
+
+    puts("Enter another string, please.");
+    fgets(words, STLEN, stdin);
+
+    printf("Your string twice (puts(), then fputs()):\n");
+    puts(words);
+    fputs(words, stdout);
+    puts("Done.");
 
     return 0;
 }
