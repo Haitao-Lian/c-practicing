cdd6ff629baacd5c3558ea5ab75c260656e10a6e
HaiTao-Lian <1654415895@qq.com>
2024-02-03 23:36:19 +0800

    add examples

--- c-primer-plus/string/fgest3.c
+++ c-primer-plus/string/fgest3.c
@@ -6,20 +6,20 @@ int main()
     char words[STLEN];
     int i;
 
-    puts("Enter strings (empty line to quit:)");
+    puts("Enter strings (empty line to quit):");
     while (fgets(words, STLEN, stdin) != NULL && words[0] != '\n') {
         i = 0;
-        while (words[i] != '\n' && words[i] != '\0') {
+        while (words[i] != '\n' && words[i] != '\0')
             i++;
-        }
         if (words[i] == '\n')
             words[i] = '\0';
         else
             while (getchar() != '\n')
                 continue;
+
         puts(words);
     }
-    puts("Done");
+    puts("Dones.");
 
     return 0;
 }
